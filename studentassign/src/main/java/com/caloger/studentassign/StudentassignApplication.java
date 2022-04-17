64646c9d87aec8ca6609127f70fe34ae85df01eb
mcaloger <mattcaloger@gmail.com>
2022-04-16 20:20:39 -0400

    Spring generation

---
+++ studentassign/src/main/java/com/caloger/studentassign/StudentassignApplication.java
@@ -0,0 +1,13 @@
+package com.caloger.studentassign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentassignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentassignApplication.class, args);
+	}
+
+}
---
+++ studentassign/src/test/java/com/caloger/studentassign/StudentassignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caloger.studentassign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentassignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
