98f0978de8b7167c313d41868ac3c3fa5dddfc32
mcaloger <mattcaloger@gmail.com>
2022-06-24 23:42:07 -0400

    Update AssignmentModel.cs

--- StudentAssignMVC/Assignment/AssignmentModel.cs
+++ StudentAssignMVC/Assignment/AssignmentModel.cs
@@ -1,6 +1,14 @@
-﻿namespace StudentAssignMVC.Assignment
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentAssignMVC.Assignment
 {
     public class AssignmentModel
     {
+        int id { get; set; }
+        [Required]
+        string title { get; set; }
+        string description { get; set; }
+        DateTime DueDate { get; set; }
     }
 }
+ 
